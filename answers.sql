bb19a5403773e0a125f3c291118dcd8db8958242
Keren Hapuch <kerenhapuch5@gmail.com>
2025-05-16 13:53:19 +0300

    week7 assignment

--- answers.sql
+++ answers.sql
@@ -1,23 +1,41 @@
 #Question1
 
-SELECT 
-  OrderID,
-  CustomerName,
-  LTRIM(RTRIM(value)) AS Product
-FROM ProductDetail
-CROSS APPLY STRING_SPLIT(Products, ',');
-
-
-#Question2
--- Table 1: Orders (removes partial dependency)
-SELECT DISTINCT 
-  OrderID, 
-  CustomerName
-FROM OrderDetails;
-
--- Table 2: OrderItems (normalized)
-SELECT 
-  OrderID, 
-  Product, 
-  Quantity
-FROM OrderDetails;
+CREATE TABLE ProductDetail (
+OrderID INT,
+CustomerName VARCHAR(100),
+Products VARCHAR(100)
+);
+INSERT INTO ProductDetail(OrderID, CustomerName, Products)
+VALUES
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
+-- Question 2
+CREATE TABLE orders(
+OrderID INT PRIMARY KEY,
+customerName VARCHAR(100)
+);
+INSERT INTO orders (OrderID, CustomerName)
+VALUES
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
+-- Product table
+CREATE TABLE product(
+product_id INT primary key,
+productName varchar(100),
+quantity INT,
+order_id INT,
+foreign key(order_id) references orders(OrderID)
+);
+insert into product(product_id,productName,quantity,order_id)
+values
+(1,'laptop',2,101),
+(2,'Mouse',1,101),
+(3,'Tablet',3,102),
+(4,'Keyboard',2,102),
+(5,'Mouse',1,102),
+(6,'Phone',1,103);
